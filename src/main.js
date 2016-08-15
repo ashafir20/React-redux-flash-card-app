5f0d5c57d33d86ffba9eb2db50c7c6a0369a8214
aviv <aviv@innovid.com>
2016-08-15 14:50:47 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+import * as reducers from './reducers';
+
+reducers.routing = routerReducer;
+
+import * as localStore from './localStore';
+
+import App from './components/App';
+import VisibleCards from './components/VisibleCards';
+import NewCardModal from './components/NewCardModal';
+import EditCardModal from './components/EditCardModal';
+
+const store = createStore(combineReducers(reducers), localStore.get());
+const history = syncHistoryWithStore(browserHistory, store);
+
+function run() {
+    let state = store.getState();
+    localStore.set(state, ['decks', 'cards']);
+    ReactDOM.render(
+        <Provider store={store}>
+            <Router history={history}>
+                <Route path="/" component={App}>
+                    <Route path="/deck/:deckId" component={VisibleCards}>
+                        <Route path="/deck/:deckId/new" component={NewCardModal} />
+                        <Route path="/deck/:deckId/edit/:cardId" component={EditCardModal} />
+                    </Route>
+                </Route>
+            </Router>
+        </Provider>,
+        document.getElementById("root"));
+}
+
+run();
+
+store.subscribe(run);
+
+window.show = () => store.dispatch(showAddDeck());
+window.hide = () => store.dispatch(hideAddDeck());
+window.add = () => store.dispatch(addDeck(new Date().toString()));
